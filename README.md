98dfef076cce728eeb5f06ddc751bd7e607e77cd
Cheng Yin <chengyinlye@gmail.com>
2023-07-17 15:09:14 +0800

    Updated Toggle

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # AutoClicker
 
-Based upon
-
 Setup
 pip install -r requirements.txt
+
+The provided code is a Python script for recording and replaying mouse actions. It uses the keyboard library to start and stop the recording process and the pynput.mouse library to capture mouse events such as clicks, scroll movements, and mouse position.
+
+When executed, the script allows you to start the recording by pressing the 'q' key and stop it by pressing the 'e' key. The recorded mouse actions are saved to a file called "recorded_actions.txt". The script also includes a function to load and replay the recorded actions.
+
+To use the script, you can follow these steps:
+
+Press 'q' to start recording.
+Perform mouse actions such as clicks, scrolls, and movements.
+Press 'e' to stop recording and save the actions to "recorded_actions.txt".
+Run the script again to replay the recorded actions from the file.
 
--- ReplayActions.py
+++ ReplayActions.py
@@ -31,12 +31,53 @@ else:
 #Released+Button.left  
 
 mouse = MouseController()
+
+paused = False  # Flag to indicate if the script is paused
+
+def toggle_pause():
+    global paused
+    paused = not paused
+    print("Script paused" if paused else "Script resumed")
+
+# Register a hotkey to toggle pause/resume
+keyboard.add_hotkey('p', toggle_pause)
+
 for action in actions:
+    if paused:
+        time.sleep(0.1)  # Pause the script execution when it's paused
+        continue
+
     if action.startswith("MOVE"):
         x, y = map(int, action.split(',', 1)[1].strip("() ").split(","))
-        mouse.position = (x,y)
+        mouse.position = (x, y)
         print(mouse.position)
-
+    elif action.startswith("SUD"):
+        mouse.scroll(0, -1)
+    elif action.startswith("SUP"):
+        mouse.scroll(0, 1)
+    elif action.startswith("Clicked"):
+        _, button_coords = action.split("+")
+        if button_coords.startswith("Button.left"):
+            coordinates = button_coords.split(",", maxsplit=1)[1].strip()
+            x, y = map(int, coordinates.strip("() ").split(","))
+            mouse.position = (x, y)
+            mouse.press(Button.left)
+        elif button_coords.startswith("Button.right"):
+            coordinates = button_coords.split(",", maxsplit=1)[1].strip()
+            x, y = map(int, coordinates.strip("() ").split(","))
+            mouse.position = (x, y)
+            mouse.press(Button.right)
+    elif action.startswith("Released"):
+        _, button_coords = action.split("+")
+        if button_coords.startswith("Button.left"):
+            coordinates = button_coords.split(",", maxsplit=1)[1].strip()
+            x, y = map(int, coordinates.strip("() ").split(","))
+            mouse.position = (x, y)
+            mouse.release(Button.left)
+        elif button_coords.startswith("Button.right"):
+            coordinates = button_coords.split(",", maxsplit=1)[1].strip()
+            x, y = map(int, coordinates.strip("() ").split(","))
+            mouse.position = (x, y)
+            mouse.release(Button.right)
     # Pause for a short interval between actions (adjust as needed)
     # time.sleep(0.05)
-
