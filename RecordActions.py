708a5e2bc19dd94fd8dc832ec12b0dafe0ce010a
Cheng Yin <chengyinlye@gmail.com>
2023-07-17 15:09:05 +0800

    Updated toggle resume

--- RecordActions.py
+++ RecordActions.py
@@ -1,13 +1,7 @@
 import keyboard
 import os
-import signal
-from pynput.mouse import Controller
+from pynput.mouse import Controller, Listener
 import time
-# MOVE = Mouse Movement X,Y
-# SUP = Scroll Up
-# SUD = Scroll Down
-# Clicked+Button.left
-# Released+Button.left
 
 recording = False
 recordAction = []
@@ -16,17 +10,17 @@ print_mouse_position = False
 
 def start_recording():
     global recording, print_mouse_position
-    #without sleep, instantly record 
     time.sleep(0.05)
-    recordAction.clear()  # Clear the previous recorded actions
+    recordAction.clear()
     recording = True
     print_mouse_position = True
 
-    # Record the mouse position and actions continuously until recording is stopped
+    mouse_listener = Listener(on_click=on_click, on_scroll=on_scroll)
+    mouse_listener.start()
+
     while recording:
         record_mouse_position()
         
-        # Check if 'e' key is pressed to stop recording
         if keyboard.is_pressed('e'):
             stop_recording()
             break
@@ -37,11 +31,6 @@ def stop_recording():
     print_mouse_position = False
     print("Saved")
     save_actions_to_file()
-    # Get the process ID (PID) of the current Python process
-    current_pid = os.getpid()
-
-    # Terminate the Python process to end program
-    os.kill(current_pid, signal.SIGTERM)
 
 def save_actions_to_file():
     file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "recorded_actions.txt")
@@ -68,9 +57,6 @@ def on_scroll(x, y, dx, dy):
         recordAction.append(f"{action},({x}, {y})")
 
 keyboard.add_hotkey('q', start_recording)
-
-# Start the keyboard listener
 keyboard.wait('esc')
-
-# Save the recorded actions before exiting
 save_actions_to_file()
+#qeqeqeqeqe
